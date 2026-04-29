7a9ef272cb784550d4a3e1a541dd5bcc510047d7
CookieUzen <uzen@cookieuz.io>
2026-04-30 00:16:01 +0900

    codecompanion

--- config/lsp.nix
+++ config/lsp.nix
@@ -107,5 +107,52 @@
   plugins.copilot-chat.enable = true;
 
   plugins.trouble.enable = true;
+
+  # AI stuff
+  plugins.codecompanion = {
+    enable = true;
+    settings = {
+      adapters = {
+        http = {
+          ollama = {
+            __raw = ''
+              function()
+                return require('codecompanion.adapters').extend('ollama', {
+                    env = {
+                        url = "http://127.0.0.1:11434",
+                    },
+                    schema = {
+                        model = {
+                            default = 'qwen3.6:latest',
+                        },
+                        num_ctx = {
+                            default = 32768,
+                        },
+                    },
+                })
+              end
+            '';
+          };
+        };
+      };
+      opts = {
+        log_level = "TRACE";
+        send_code = true;
+        use_default_actions = true;
+        use_default_prompts = true;
+      };
+      strategies = {
+        agent = {
+          adapter = "ollama";
+        };
+        chat = {
+          adapter = "ollama";
+        };
+        inline = {
+          adapter = "ollama";
+        };
+      };
+    };
+  };
 }
 
